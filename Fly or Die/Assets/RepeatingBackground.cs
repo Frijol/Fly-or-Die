572c5ee82c816d64d0611638c84bf99cdaca8099
Andre Stackhouse <CaptainStack@outlook.com>
2019-11-01 23:43:46 -0700

    Add background, charcter, and scrolling

---
+++ Fly or Die/Assets/RepeatingBackground.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBackground : MonoBehaviour
+{
+    public float speed;
+
+    public float endX;
+    public float startX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Coords");
+        Debug.Log(gameObject.GetComponent<SpriteRenderer>().bounds.size.x);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+        if (transform.position.x <= endX) {
+            Vector2 pos = new Vector2(startX, transform.position.y);
+            transform.position = pos;
+        }
+    }
+}
