7de04f032ab037e29de5893a64fd7932276b44ec
Andre Stackhouse <CaptainStack@outlook.com>
2019-11-10 17:28:49 -0800

    Add jump physics

--- Fly or Die/Assets/Player.cs
+++ Fly or Die/Assets/Player.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D rigidBody2D;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody2D = GetComponent<Rigidbody2D>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        HandleInput();
+    }
+
+    private void HandleInput() {
+        if (Input.GetButtonDown("Jump")) {
+            rigidBody2D.AddForce(Vector2.up * 8, ForceMode2D.Impulse);
+        }
     }
 }
