1d47933a65884549e7ccbd516e9159b3314a6c50
Andre Stackhouse <CaptainStack@outlook.com>
2019-11-10 20:59:46 -0800

    Create obstacle and losing logic

---
+++ Fly or Die/Assets/Obstacle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Obstacle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Arnett") {
+            SceneManager.LoadScene("Game");
+        }
+    }
+}
