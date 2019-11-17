92c35944589e6dfa2054955d0cafec3c005e7912
Andre Stackhouse <CaptainStack@outlook.com>
2019-11-16 19:24:31 -0800

    Create obstacle prefab and spawning logic

--- Fly or Die/Assets/ObstacleSpawner.cs
+++ Fly or Die/Assets/ObstacleSpawner.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
+    public float respawnFrequency;
+    private float timeUntilNextSpawn;
+    public GameObject obstaclePrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeUntilNextSpawn = respawnFrequency;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeUntilNextSpawn -= Time.deltaTime;
+
+        if (timeUntilNextSpawn <= 0f) {
+            timeUntilNextSpawn = respawnFrequency;
+            Instantiate(obstaclePrefab, this.transform.position, Quaternion.identity);
+        }
     }
 }
