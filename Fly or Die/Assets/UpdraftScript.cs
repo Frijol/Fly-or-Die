9148a1c0c4f0cba07d627556844758763b31345f
Christopher Proteau <christopherproteau@gmail.com>
2019-11-23 12:06:29 -0800

    forgot some files for the last commit

---
+++ Fly or Die/Assets/UpdraftScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdraftScript : MonoBehaviour
+{
+    public float speed;
+    private Collider2D[] playerColliders;
+    private Collider2D updraftCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Fetch player and updraft colliders
+        playerColliders = GameObject.FindWithTag("Player").GetComponents<Collider2D>();
+        updraftCollider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+}
