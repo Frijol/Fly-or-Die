d27e25c10cc8bf5c311a56fda731e1f91f6f7166
Andre Stackhouse <CaptainStack@outlook.com>
2019-11-22 21:35:14 -0800

    Add score object and script

---
+++ Fly or Die/Assets/ScoreScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreScript : MonoBehaviour
+{
+    public float score;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score += Time.deltaTime;
+        GetComponent<UnityEngine.UI.Text>().text = Mathf.Floor(score).ToString();
+        
+    }
+}
