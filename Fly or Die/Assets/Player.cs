c15c2398eddbecbdc57eeb8d3d0496e10850acec
Andre Stackhouse <CaptainStack@outlook.com>
2019-11-30 19:32:51 -0800

    Disable gliding while on the ground.

--- Fly or Die/Assets/Player.cs
+++ Fly or Die/Assets/Player.cs
@@ -35,7 +35,7 @@ public class Player : MonoBehaviour
             isOnGround = false;
         }
 
-        if (Input.GetButton("Glide"))
+        if (Input.GetButton("Glide") && !isOnGround)
         {
             isGliding = true;
             rigidBody2D.drag = 10;
