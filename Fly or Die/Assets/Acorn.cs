2a0919b8ba1f240e270540fd74cd9d8facf8d190
Andre Stackhouse <CaptainStack@outlook.com>
2019-11-30 22:22:03 -0800

    Add sprite, prefab, and spawner

---
+++ Fly or Die/Assets/Acorn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Acorn : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Arnett") {
+            var scoreScript = GameObject.Find("Score").GetComponent<ScoreScript>();
+            scoreScript.score += 10; // Score will be doubled because the player has two colliders
+            Destroy(transform.gameObject);
+        }
+    }
+}
