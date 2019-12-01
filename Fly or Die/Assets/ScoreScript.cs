886a293dded173d69ccde500dd27265d7b155374
Christopher Proteau <christopherproteau@gmail.com>
2019-11-30 19:35:21 -0800

    add a score multiplier

--- Fly or Die/Assets/ScoreScript.cs
+++ Fly or Die/Assets/ScoreScript.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class ScoreScript : MonoBehaviour
 {
     public float score;
+    public float multiplier;
+
     void Start()
     {
-        
+        multiplier = 1f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        score += Time.deltaTime;
+        score += Time.deltaTime * multiplier;
         GetComponent<UnityEngine.UI.Text>().text = Mathf.Floor(score).ToString();
-        
     }
 }
